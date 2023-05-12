02d527beff03fcdf2041a6a044d9cb13b49eaf5a
maciejlastowka@gmail.com <120501693+MaciejLastowka@users.noreply.github.com>
2023-05-12 08:26:12 +0200

    Game works.

--- src/main/java/org/hangman/Hangman.java
+++ src/main/java/org/hangman/Hangman.java
@@ -9,6 +9,7 @@ public class Hangman {
     List<String> words = List.of("car", "computer", "programming", "football");
     String word;
     char[] userWord;
+    int lives = 5;
 
     public void play() {
         Scanner scanner = new Scanner(System.in);
@@ -19,12 +20,10 @@ public class Hangman {
         userWord = new char[word.length()];
         Arrays.fill(userWord, '_');
 
-        System.out.println(userWord);
-
         while (!gameEnded()) {
             System.out.println(userWord);
             System.out.println();
-            System.out.println("Podaj litere");
+            System.out.println("Provide a letter: ");
 
             char letter = scanner.nextLine().charAt(0);
 
@@ -36,14 +35,32 @@ public class Hangman {
     }
 
     private void checkLetter(char letter) {
+        boolean foundLetter = false;
+
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == letter) {
                 userWord[i] = letter;
+                foundLetter = true;
             }
+        }
+        if (!foundLetter) {
+            System.out.println("Wrong letter :( ");
+            int live = lives--;
+            System.out.println("You have " + live + " lives left");
+            System.out.println("============================"+ "\n");
         }
     }
 
     private boolean gameEnded() {
-        return false;
+
+        if (lives == 0) {
+            System.out.println("You lost. Try next time!");
+            return true;
+        } else if (word.equals(String.valueOf(userWord))) {
+            System.out.println("Congratulation! You won!");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
